aadca8803e765c4ee9e02194568aebfcdcd0b76f
mintoua <toupkandi.mintoua@esprit.tn>
2023-08-13 20:21:21 +0100

    services en injection de dependance

---
+++ src/app/services/post.service.ts
@@ -0,0 +1,13 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PostService {
+
+  postList: Array<any> = [
+    {id:1, postTitle: "Post 1"},
+    {id:2, postTitle: "Post 2"}
+  ]
+  constructor() { }
+}
