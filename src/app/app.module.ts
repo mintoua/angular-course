13a07d1ecc195d6d15183f96b6a679ea0b4b6849
mintoua <toupkandi.mintoua@esprit.tn>
2023-08-19 21:23:10 +0100

    updated

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { CoursDetailsComponent } from './cours-details/cours-details.component';
-import { HeaderComponent } from './layout/header/header.component';
-import { FooterComponent } from './layout/footer/footer.component';
-import { NavbarComponent } from './layout/navbar/navbar.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { NavbarComponent } from './navbar/navbar.component';
 import { PostComponent } from './post/post.component';
 import {FormsModule} from "@angular/forms";
 import { BlogComponent } from './blog/blog.component';
 import { HomeComponent } from './home/home.component';
-import {AppRoutingModule} from "./app-routing.module";
+import { AppRoutingModule } from "./app-routing.module";
 import { CoursListComponent } from './cours-list/cours-list.component';
-import {RouterLink, RouterOutlet} from "@angular/router";
+import {RouterLink, RouterLinkActive, RouterOutlet} from "@angular/router";
 
 @NgModule({
   declarations: [
@@ -30,7 +30,8 @@ import {RouterLink, RouterOutlet} from "@angular/router";
     BrowserModule,
     FormsModule,
     RouterLink,
-    RouterOutlet
+    RouterOutlet,
+    RouterLinkActive
   ],
   providers: [],
   bootstrap: [AppComponent]
